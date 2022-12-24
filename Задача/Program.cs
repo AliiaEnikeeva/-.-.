e2f40d827fa63fd79e1d6f74aa443743b1f1f1d4
Enikeeva Aliya <aliaa@mail.ru>
2022-12-24 22:49:30 +0500

    добавлено частичное решение задачи

--- Задача/Program.cs
+++ Задача/Program.cs
@@ -2,3 +2,31 @@
 // длина которых меньше либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
 // либо задать на старте выполнения алгоритма.
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись искючительно массивами.
+
+// 32.Задать массив из 8 целых элементов и вывести их на экран
+
+string[] arr1 = {"hello", "2", "world", ":-)"};
+string[] arr2 = {"1234", "1567", "-2", "computer science"};
+string[] arr3 = {"russia", "Denmark", "Kazan"};
+
+int max_word_length = 3;
+
+string[] GetShortWordsArray(string[] array)
+{
+    int length = array.Length;
+    string[] result = new string[length];
+    int count = 0;
+
+    for (int i=0;i<length;i++)
+    {
+        if(array[i].Length<=max_word_length)
+        {
+            result[count]=array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref result, count);
+    return result;
+}
+
+
